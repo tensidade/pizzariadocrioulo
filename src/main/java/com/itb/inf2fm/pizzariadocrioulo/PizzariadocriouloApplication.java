b38e5401e3d54116ddeb6d727138c3455ca208fd
tensidade <rm99731@estudante.fieb.edu.br>
2026-05-28 07:34:19 -0300

    criado ambiante de dev

---
+++ src/main/java/com/itb/inf2fm/pizzariadocrioulo/PizzariadocriouloApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzariadocrioulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariadocriouloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariadocriouloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2fm/pizzariadocrioulo/PizzariadocriouloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzariadocrioulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariadocriouloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
