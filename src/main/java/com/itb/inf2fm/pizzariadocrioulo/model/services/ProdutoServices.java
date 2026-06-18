6be615e2bb532065202be84e776a596f3c21cc66
tensidade <rm99731@estudante.fieb.edu.br>
2026-06-18 15:30:04 +0000

    camada model services em construção

---
+++ src/main/java/com/itb/inf2fm/pizzariadocrioulo/model/services/ProdutoServices.java
@@ -0,0 +1,12 @@
+package com.itb.inf2fm.pizzariadocrioulo.model.services;
+
+import java.util.list;
+import java.util.arraylist;
+
+public class ProdutoServices {
+
+    // lista temporaria de produtos 
+
+    private static list<produtos> PRODUTOS = new arraylist<>(); 
+
+}
